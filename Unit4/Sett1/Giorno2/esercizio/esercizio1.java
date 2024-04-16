b70e56bbce961424c6d90666b54d1fa46e076612
fdinocera@libero.it <fdinocera@libero.it>
2024-04-16 15:05:48 +0200

    esercizio unit 4, settimana 1, giorno 2

---
+++ Unit4/Sett1/Giorno2/esercizio/esercizio1.java
@@ -0,0 +1,22 @@
+public class esercizio1 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+
+    boolean stringaPariDispari(String s) {
+
+        if (s.length() % 2 == 1) {
+            //dispari
+            return false;
+        } else {
+            //pari
+            return true;
+        }
+    }
+
+    void annoBisestile() {
+
+    }
+}
