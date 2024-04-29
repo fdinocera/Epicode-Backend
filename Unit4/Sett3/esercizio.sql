44ce8346468b66bb51b3ae675d3d6d237aaf07c2
fdinocera <38116029+fdinocera@users.noreply.github.com>
2024-04-29 18:10:38 +0200

    Add files via upload

--- Unit4/Sett3/esercizio.sql
+++ Unit4/Sett3/esercizio.sql
@@ -1,22 +1,50 @@
 
+-- 1)
+SELECT * FROM public.clienti where nome='mario';
 
+-- 2)
+select nome, cognome from public.clienti where anno_di_nascita='1982';
 
---SELECT * FROM public.clienti where nome='mario';
-
---select nome, cognome from public.clienti where anno_di_nascita='1982';
-
-
+-- 3)
 select * from public.fatture where iva='20';
 
-select * from public.prodotti where EXTRACT (YEAR FROM data_attivazione)='2017';
+-- 4)
+select * from public.prodotti where EXTRACT (YEAR FROM data_attivazione)='2017' 
+and (in_produzione=true or in_commercio=true);
 
-
---select * from public.fatture f  where f.importo<1000;
-
-select * from public.fatture f  inner join public.clienti c  on f.id_cliente=c.numero_cliente
+-- 5)
+select * from public.fatture f inner join public.clienti c 
+on f.id_cliente=c.numero_cliente
 where f.importo<1000;
 
-
-select numero_fattura, importo, iva, data_fattura, o.denominazione from public.fatture f inner join public.fornitori o 
+-- 6)
+select numero_fattura, importo, iva, data_fattura, o.denominazione 
+from public.fatture f inner join public.fornitori o 
 on f.numero_fornitore=o.numero_fornitore;
+
+-- 7)
+select extract (year from data_fattura) anno, count(*) numero_fatture
+from public.fatture where iva='20'
+group by anno;
+
+-- 8)
+select extract (year from data_fattura) anno, count(*) numero_fatture, sum(importo) importo_totale
+from public.fatture 
+group by anno;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
